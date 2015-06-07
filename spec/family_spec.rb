b6c07bcf3868785dba7e87184bb2ce2674fc9dc8
Jade McGough <jade@thezets.com>
2015-06-06 21:01:41 -0700

    add Family#add_member test

---
+++ spec/family_spec.rb
@@ -0,0 +1,16 @@
+describe "Family" do
+
+  before(:each) do
+    @family = Family.new
+  end
+
+  describe "#add_member" do
+
+    it "adds a new family member" do
+      @family.add_member("Nancy")
+      expect(@family.members.length).to eq(1)
+    end
+
+  end
+
+end
