332cc329b747b2578fc320d651bd2a9d3a9fd4f8
Jade McGough <jade@thezets.com>
2015-06-06 21:40:20 -0700

    allow adding member with parent

--- family.rb
+++ family.rb
@@ -1,11 +1,13 @@
 class Family
   attr_reader :members
 
+  @@memberClass = Struct.new("Member", :name, :parent)
+
   def initialize
     @members = []
   end
 
-  def add_member(person)
-    @members << person
+  def add_member(person, parent: nil)
+    @members << @@memberClass.new(person, parent)
   end
 end
