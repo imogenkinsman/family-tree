a33099f7d69815910c301b14526f363095227cd9
Jade McGough <jade@thezets.com>
2015-06-06 22:00:22 -0700

    DRY member lookup

--- family.rb
+++ family.rb
@@ -8,12 +8,18 @@ class Family
   end
 
   def add_member(name, parent_name: nil)
-    parent = @members.find{ |member| member.name == parent_name}
+    parent = get_member_by_name(parent_name)
     @members << @@memberClass.new(name, parent)
   end
 
   def grandparent_of(name)
-    member = @members.find{ |member| member.name == name }
+    member = get_member_by_name(name)
     member.parent.parent.name
+  end
+
+  private
+
+  def get_member_by_name(name)
+    @members.find{ |member| member.name == name }
   end
 end
